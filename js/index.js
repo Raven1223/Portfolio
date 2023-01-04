ed06dc1e027785427c73d6af2dcac3c9c2ec1102
Shaun Zulu <Zulu.shaun4@gmail.com>
2023-01-03 22:11:55 -0600

    <Added Name, Email, and Message section for user to enter>

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ let footer = document.querySelector("footer");
 
 let copyright = document.createElement("p");
 
-copyright.innerHTML =`Shaun Zulu ${thisYear}`
+copyright.innerHTML =`Shaun Zulu ${thisYear}`;
 
 footer.appendChild(copyright);
 
@@ -19,7 +19,52 @@ let skillsList = skillSection.querySelector("ul");
 for (i=0; i < skills.length; i++){
 let skill = document.createElement("li");
 
-skill.innerHTML = [i];
+skill.innerHTML = skills[i];
 
 skillsList.appendChild(skill);
+}
+
+let messageForm = document.getElementsByName("leave_message");
+
+
+messageForm.addEventListener("submit", function(event){
+event.preventDefault();
+let messageForm = event.target;
+
+let enterName = event.target.name;
+let enterEmail = event.target.email;
+let enterMessage = event.target.message;
+
+console.log(enterName);
+console.log(EnterEmail);
+console.log(EnterMessage);
+
+			//Display Messages in List
+let messageSection = document.getElementById("#messages");
+
+let messageList = messageSection.querySelector("ul");
+
+let newMessage = document.createElement("li");
+
+newMessage.innerHTML =`<a href= "mailto:${enterEmail}">${enterName}</a>
+			<span>${messageInput}</span>
+`;
+}
+				//Removing Button
+let removeButton.document.createElement("button");
+
+removeButton.type= "button";
+
+removeButton.addEventListener("click", (event){
+
+let entry = removeButton.parentNode;
+entry.remove();
+
+messageList.appendChild(removeButton);
+
+messageList.appendChild(newMessage);
+
+
+
+messageForm.reset();
 }
